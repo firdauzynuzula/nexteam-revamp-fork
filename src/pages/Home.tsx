e7c307a536a2420974bfadd5e00bb947dc467659
firdauzy <nuzulafirdhauzy@gmail.com>
2024-08-06 13:25:32 +0700

    update:header component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,8 +3,7 @@ export const Home = () => {
         <>
             <div className="w-3/4 mx-auto sm:w-full min-h-10 text-center mt-10">
                 <h1>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Laudantium, impedit.
+                    Hello world
                 </h1>
             </div>
         </>
--- src/partials/Header.tsx
+++ src/partials/Header.tsx
@@ -1,7 +1,8 @@
 export const Header = () => {
     return (
         <>
-            <header className="container mx-auto w-5/6 max-h-20 mt-5">
+            {/* header / navbar */}
+            <header className="container mx-auto w-5/6 min-h-[400px] mt-5">
                 <nav className="flex justify-between px-10 text-semibold">
                     <div className="logo">
                         <img
@@ -22,6 +23,22 @@ export const Header = () => {
                         </button>
                     </div>
                 </nav>
+
+                {/* hero section */}
+                <div className="min-h-[100px] flex flex-col justify-center items-center px-5 mt-2">
+                    {/* text section */}
+                    <div className="sm:w-1/2 text-wrap text-center order-1">
+                        <h1 className="text-2xl pt-5 sm:text-4xl font-bold">Lorem ipsum dolor sit amet Consectetur</h1>
+                    </div>
+                    {/* image section */}
+                    <div className="p-5 order-2">
+                        <img
+                            src="/src/assets/biryani.png"
+                            alt="nasi-briyani"
+                            className="max-h-[300px] bg-cover"
+                        />
+                    </div>
+                </div>
             </header>
         </>
     );
